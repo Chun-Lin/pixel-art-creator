336dec87b093edc91a6d05edd6d3ab0b3d136439
Chun-Lin <wulin40063@gmail.com>
2020-04-29 17:55:54 +0800

    Updates

---
+++ precache-manifest.3f55444bd2d31ba8b6396ec8bf4136fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e11e9f056dea3a806444e4d590cf51",
+    "url": "/pixel-art-creator/index.html"
+  },
+  {
+    "revision": "04d607dfd83aff057c96",
+    "url": "/pixel-art-creator/static/css/main.984b8153.chunk.css"
+  },
+  {
+    "revision": "001b68094bdd30e7af47",
+    "url": "/pixel-art-creator/static/js/2.8611ac46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixel-art-creator/static/js/2.8611ac46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d607dfd83aff057c96",
+    "url": "/pixel-art-creator/static/js/main.aa5ab42a.chunk.js"
+  },
+  {
+    "revision": "a5e9fdb7f75934c69d60",
+    "url": "/pixel-art-creator/static/js/runtime-main.1eaf2674.js"
+  }
+]);
